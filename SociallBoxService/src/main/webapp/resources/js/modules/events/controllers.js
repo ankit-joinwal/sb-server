c26650e084eb3fb4c6a88d2c698a0661bc08822b
unknown <rahulsingh.leo91@gmail.com>
2016-05-01 22:39:18 -0700

    create event changes

--- SociallBoxService/src/main/webapp/resources/js/modules/events/controllers.js
+++ SociallBoxService/src/main/webapp/resources/js/modules/events/controllers.js
@@ -3,7 +3,7 @@
 var app = angular.module('Events',['textAngular']);
 
 app.controller('EventsController',
-		['$window','$scope', '$rootScope', '$routeParams','$location','AuthenticationService','EventService',
+		['$window','$scope', '$rootScope','$cookieStore', '$routeParams','$location','AuthenticationService','EventService',
         function ($window,$scope, $rootScope, $routeParams,$location,AuthenticationService,EventService) {
 		
 		   $scope.eventDetails = '<h2>Enter event details like features,ticket booking url etc</h2>';
@@ -12,21 +12,143 @@ app.controller('EventsController',
 				$window.location.href = "/SociallBox/eo/home#/events/new";
 			};
 			
-			$scope.getDetails = function(){
-				var eventDetails = $scope.eventDetails;
-				alert('htmlContent : '+eventDetails);
+			$scope.myevents_upcoming = function (){
+				AuthenticationService.isUserLoggedIn()
+				.then(function(response){
+					EventService.myevents_upcoming()
+					.then(function(eventResponse){
+						$scope.myEventsData_upcoming =eventResponse.data.data;
+						});
+					
+				}).catch(function(response){
+					console.log("Inside event service controller Response :"+response.status);
+//					$window.location.href = "/SociallBox/eo/login";
+		
+				});
+			}
+			
+			$scope.myevents_past = function (){
+				AuthenticationService.isUserLoggedIn()
+				.then(function(response){
+					EventService.myevents_past()
+					.then(function(eventResponse){
+						$scope.myEventsData_past =eventResponse.data.data;
+						});
+					
+				}).catch(function(response){
+					console.log("Inside event service controller Response :"+response.status);
+//					$window.location.href = "/SociallBox/eo/login";
+		
+				});
+			}
+			
+			
+			
+			//$scope.tagNames = [];
+			var tags = [],isFree,startDate,endDate;
+			
+			$scope.uploadDataForReview =function(){
+				if($('#IsFreeButton button.active').text()=="YES")
+					isFree ="true";
+					else
+						isFree="false";
+				var tagName ={};
+				$("#tags button").each(function(){
+					var buttonClass =$(this).attr("class");
+					if (buttonClass == "btn btnactive"){
+						var tagText = $(this).text();
+						tagName = { 
+								"name" : tagText,
+							};
+						tags.push(tagName);
+					};
+				});
+				
+					 startDate =$("#startDate").val();
+					 endDate = $("#endDate").val();
+					
+				   $("#reviewEventStartDate").text(startDate);
+				   $("#reviewEventEndDate").text(endDate);
+				   
+				   for(var i =0;i<tags.length;i++){
+						$(".reviewTagNames").append('<button type="button" class="btn btnactive">'+tags[i].name+'</button>');
+						
+					};
+					if (isFree == "true")
+					$(".reviewFreeButton").append('<button type="button" class="btn btnactive">Yes</button>');
+					else
+					$(".reviewFreeButton").append('<button type="button" class="btn btnactive">NO</button>');
+					
+			}; 
+			
+			$scope.createEvent = function(){
+				AuthenticationService.isUserLoggedIn()
+				.then(function(response){
+					var eventDetails = $scope.eventDetails;
+					var eventTitle =$scope.eventTitle;
+					startDate =$("#startDate").val();
+					endDate = $("#endDate").val();
+				//	var profileId = $routeParams.profileId;
+					var locationName = $scope.eventLocation;
+					var locLat = $scope.event_place_lat;
+					var locLng = $scope.event_place_lng;
+					var locality = $scope.event_address_components[1].short_name;
+					console.log('Name :'+locationName);
+					console.log('Lat :'+locLat);
+					console.log('Long :'+locLng);
+					console.log('shortName' + shortName);
+				
+					
+					var createEventRequest = ' { '+
+											 ' 	"title" 		 : "' +eventTitle+ 	'",'+
+											 '	"description" 	 : "' + eventDetails+		'", '+
+											 '	"event_details"  : {'+
+																	' "location"		: {'+
+																	'						"name" 	: "' +locationName+'" ,'+
+																	'						"locality" 		: "' +locality+'" ,'+
+																	'						"longitude" 	: "' +locLat+'" ,'+
+																	'						"lattitude"	: "' +locLng+'" ,'+
+																	'				 	  },'+
+																	' },'+
+											'	"tags" 		     : "' +tags + '",'+
+											'	"startDate" 	 : "' +startDate + '", '+
+											'	"endDate" 		 : "' +endDate + '" , '+
+											'	"profileId" 	 : "' +profileId + '" , '+
+											'   "is_free"        : "' +isFree + '"  '+
+									'}';
+					
+					
+					EventService.createEvent(createEventRequest)
+					.then(function(eventResponse){
+						var eventId = eventResponse.data.id;
+						var eventPic = $scope.eventPic;
+				        console.log('event Pic :' );
+				        console.dir(eventPic);
+				        if(eventPic != null){
+				        	EventService.uploadEventPhoto(eventId,eventPic)
+					        .then(function(uploadResponse){
+					        	if(uploadResponse.status == 201){
+						        	console.log('Uploaded event Pic');
+						        	
+					        	}
+					         })
+					        .catch(function(uploadResponse){
+								console.log('Error in uploading event photo. Response :'+uploadResponse.status);
+							});
+				        }
+					})
+					.catch(function(eventResponse){
+						console.log('Error in creating company profile . Response :'+createResponse.status);
+					});
+				})
+				.catch(function(response){
+					console.log('Inside EventController.createEvent to create event. Response :'+response.status);
+					//console.log('Inside EventController.createEvent to create event. Response :'+response.status);
+					//$window.location.href = "/SociallBox/eo/login";
+				});
 			};
 			
-			$scope.getEventLocation = function(){
-				var locationName = $scope.eventPlace;
-				var locLat = $scope.event_place_lat;
-				var locLng = $scope.event_place_lng;
-				var addressComponents = JSON.stringify($scope.event_address_components);
-				console.log('Name :'+locationName);
-				console.log('Lat :'+locLat);
-				console.log('Long :'+locLng);
-				console.log('AddressComponents' + JSON.stringify(JSON.parse(addressComponents),null,2));
-			}
+			
 		}
 		
 		]);
--- SociallBoxService/src/main/webapp/resources/js/modules/events/services.js
+++ SociallBoxService/src/main/webapp/resources/js/modules/events/services.js
@@ -7,6 +7,159 @@ app.factory('EventService',
 	    function ($http, $cookieStore, $rootScope, $q,AuthenticationService) {
 	    	var service = {};
 	    	
+	   	 service.myevents_upcoming = function(){
+	 			var deferred = $q.defer();
+	 			var myeventsURL = "/SociallBox/api/secured/users/organizers/admins/25/events?timeline=upcoming";
+	 			return AuthenticationService.getAuthToken()
+	    		.then(function(tokenResponse){
+	    			//Extract epoch time and token from response
+	    			var epoch = tokenResponse.epoch;
+	    			var token = tokenResponse.token;
+	    			return $http({
+	 	 				method:'GET',
+	 	 				url: myeventsURL,
+	 	 	            headers: {
+	 	 	                    "Content-Type"		: 	"application/json",
+	 	 						"accept"			:	"application/json",
+	 	 	                    "X-Login-Ajax-call"	: 	'true',
+	 	 	                    "Authorization"		:	token , 
+		 	                    "X-Auth-Date" 		: 	epoch
+	 	 	            }
+	 	    		 })
+	 	    		 .then(function(response) {
+		                 if (response.status == 200) {
+		                	 console.log('My events in:'+response.status);
+		                	 deferred.resolve(response);
+		 					 return deferred.promise;
+		                 }
+	 	    		 }).catch(function(response){
+	 	    			 console.log('Unable to get my events data . Response :'+response.status);
+	 	    			 deferred.reject(response);
+	 					 return deferred.promise;
+	 	    		});
+	    		}).catch(function(tokenResponse){
+	    			//If unable to get auth token, then redirect to login page
+	    			console.log('Unable to gen token in AdminService.getPendingProfiles.Response :'+tokenResponse.status);
+	    			
+	    		});
+	 		 };
+	 		 
+	 		 service.myevents_past = function(){
+		 			var deferred = $q.defer();
+		 			var myeventsURL = "/SociallBox/api/secured/users/organizers/admins/25/events?timeline=past";
+		 			return AuthenticationService.getAuthToken()
+		    		.then(function(tokenResponse){
+		    			//Extract epoch time and token from response
+		    			var epoch = tokenResponse.epoch;
+		    			var token = tokenResponse.token;
+		    			return $http({
+		 	 				method:'GET',
+		 	 				url: myeventsURL,
+		 	 	            headers: {
+		 	 	                    "Content-Type"		: 	"application/json",
+		 	 						"accept"			:	"application/json",
+		 	 	                    "X-Login-Ajax-call"	: 	'true',
+		 	 	                    "Authorization"		:	token , 
+			 	                    "X-Auth-Date" 		: 	epoch
+		 	 	            }
+		 	    		 })
+		 	    		 .then(function(response) {
+			                 if (response.status == 200) {
+			                	 //console.log('My events past in:'+response.status);
+			                	 deferred.resolve(response);
+			 					 return deferred.promise;
+			                 }
+		 	    		 }).catch(function(response){
+		 	    			 console.log("Unable to get my events past data . Response :"+response.status);
+		 	    			 deferred.reject(response);
+		 					 return deferred.promise;
+		 	    		});
+		    		}).catch(function(tokenResponse){
+		    			//If unable to get auth token, then redirect to login page
+		    			console.log('Unable to gen token in AuthenticationService.Response :'+tokenResponse.status);
+		    			
+		    		});
+		 		 };
+	 		 
+		 		service.createEvent = function(createEventRequest){
+		    		var deferred = $q.defer();
+		    		var createEventURL = "/SociallBox/api/secured/events";
+		    		return AuthenticationService.getAuthToken()
+		    		.then(function(tokenResponse){
+		    			//Extract epoch time and token from response
+		    			var epoch = tokenResponse.epoch;
+		    			var token = tokenResponse.token;
+		    			
+		    			//Make API call to create company
+		    			return $http({
+		 	 				method:'POST',
+		 	 				url: createEventURL,
+		 	 	            data: createEventRequest,
+		 	 	            headers: {
+		 	 	                    "Content-Type"		: 	"application/json",
+		 	 		 				"accept"			:	"application/json",
+		 	 	                    "X-Login-Ajax-call"	: 	'true',
+		 	 	                    "Authorization"		:	token , 
+			 	                    "X-Auth-Date" 		: 	epoch
+		 	 	            }
+		 	    		 }).then(function(response) {
+		 	                 if (response.status == 201) {
+		 	                 	deferred.resolve(response);
+		 	                 	console.log('Event created'+response.status)
+		 	 					return deferred.promise;
+		 	                 }else{
+		 	                	 
+		 	 					 deferred.reject(response);
+		 	 					 return deferred.promise;
+		 	                 }
+		 	             });
+		    		}).catch(function(response){
+		    			//If unable to get auth token, then redirect to login page
+		    			console.log('Inside EventService.createEvent to gen token.Response :'+response.status);
+		    		});
+		    	};
+		    	
+		    	
+		    	service.uploadEventPhoto = function(eventId,eventPic){
+		    		var deferred = $q.defer();
+		    		var uploadUrl ="/SociallBox/api/secured/events/"+eventId+"/images"
+		    		return AuthenticationService.getAuthToken()
+				    		.then(function(tokenResponse){
+					    			//Extract epoch time and token from response 
+					    			var epoch = tokenResponse.epoch;
+					    			var token = tokenResponse.token;
+					    			 var fd = new FormData();
+				    		        fd.append('files', eventId);
+				    		        return $http.post(uploadUrl, fd, {
+				    		            transformRequest: angular.identity,
+				    		            headers: {'Content-Type': undefined, "X-Login-Ajax-call"	: 	'true',"accept"			:	"application/json",
+				 	 	                    "Authorization"		:	token , 
+					 	                    "X-Auth-Date" 		: 	epoch}
+				    		        })
+				    		        .then(function(uploadResponse) {
+				    		        	if (uploadResponse.status == 201) {
+					 	                 	deferred.resolve(uploadResponse);
+					 	 					return deferred.promise;
+				    		        	}else{
+					 	                	 
+					 	 					 deferred.reject(uploadResponse);
+					 	 					 return deferred.promise;
+				    		        	}
+				    		        })
+				    		        .catch(function(uploadResponse){
+				    	    			//If unable to get auth token, then redirect to login page
+				    	    			console.log('Inside EventService.uploadeventPic to upload pic.Response :'+tokenResponse.status);
+				    	    			 deferred.reject(uploadResponse);
+				 	 					 return deferred.promise;
+				    	    		});
+				    		}).catch(function(tokenResponse){
+				    			//If unable to get auth token, then redirect to login page
+				    			console.log('Inside CompanyService.uploadCompanyPhoto to gen token.Response :'+tokenResponse.status);
+				    			deferred.reject(response);
+				 				return deferred.promise;
+				    		});
+		    	};
+	    	
 	    	return service;
 	    }
 	    ]);
